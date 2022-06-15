d68f8daf74fb1dcd884cf1ed48fb656b2805808c
Ludson <ludson_ps27@hotmail.com>
2022-06-15 20:28:56 -0300

    Requisitos obrigatórios

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// const carta = document.getElementById('carta-texto"');
 const criarCarta = document.getElementById('criar-carta');
-const textoGerado = document.getElementById('texto-gerado');
+const cartaGerada = document.getElementById('carta-gerada');
+
+// Requisito 16
+const gStyle = ['newspaper', 'magazine1', 'magazine2'];
+const gSize = ['medium', 'big', 'reallybig'];
+const gRota = ['rotateleft', 'rotateright'];
+const gInc = ['skewleft', 'skewright'];
 
 function gerarCarta() {
-  const carta = document.getElementById('carta-texto');
-  if (!carta.value || carta.value === ' ') {
-    textoGerado.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  const carta = document.getElementById('carta-texto').value;
+  const cartaSeparada = carta.split(' ');
+  cartaGerada.innerHTML = '';
+  if (!carta || carta === ' ') {
+    cartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
   } else {
-    textoGerado.innerHTML = carta.value;
+    for (let index = 0; index < cartaSeparada.length; index += 1) {
+      const span = document.createElement('span');
+      cartaGerada.appendChild(span);
+      span.innerHTML = cartaSeparada[index];
+      span.className = `${gStyle[Math.floor(Math.random() * 3)]} ${gSize[Math.floor(Math.random() * 3)]} ${gRota[Math.floor(Math.random() * 2)]} ${gInc[Math.floor(Math.random() * 2)]}`;
+    }
   }
 }
 
